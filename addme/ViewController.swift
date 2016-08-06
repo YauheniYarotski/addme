1a2ede482743627e7e72b7529ed94f31380612fa
Yauheni Yarotski <yauheni_yarotski@me.com>
2016-08-06 17:34:35 +0300

    init

--- addme/ViewController.swift
+++ addme/ViewController.swift
@@ -1,25 +1,60 @@
-//
-//  ViewController.swift
-//  addme
-//
-//  Created by Yauheni Yarotski on 8/6/16.
-//  Copyright © 2016 Yauheni Yarotski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var connectionsLabel: UILabel!
+    
+    let colorService = SessionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colorService.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func redTapped(sender: AnyObject) {
+        self.changeColor(UIColor.redColor())
+        colorService.sendColor("red")
     }
-
-
+    
+    @IBAction func yellowTapped(sender: AnyObject) {
+        self.changeColor(UIColor.yellowColor())
+        colorService.sendColor("yellow")
+    }
+    
+    func changeColor(color : UIColor) {
+        UIView.animateWithDuration(0.2) {
+            self.view.backgroundColor = color
+        }
+    }
+    
+    @IBAction func retry(sender: UIButton) {
+        colorService.serviceBrowser.stopBrowsingForPeers()
+        colorService.serviceBrowser.startBrowsingForPeers()
+        
+    }
+    
+    
 }
 
+extension ViewController : SessionManagerDelegate {
+    
+    func connectedDevicesChanged(manager: SessionManager, connectedDevices: [String]) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+    
+    func colorChanged(manager: SessionManager, colorString: String) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            switch colorString {
+            case "red":
+                self.changeColor(UIColor.redColor())
+            case "yellow":
+                self.changeColor(UIColor.yellowColor())
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }
+    
+}
